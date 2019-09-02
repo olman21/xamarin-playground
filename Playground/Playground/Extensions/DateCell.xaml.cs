68c9559316fd0a4e2a2fe83549ffbdc3aab179ed
Olman <omora@gomarquis.com>
2019-09-02 12:26:59 -0600

    Implement DateCell with Bindable properties

---
+++ Playground/Playground/Extensions/DateCell.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Playground.Extensions
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateCell : ViewCell
+    {
+        public static readonly BindableProperty LabelProperty = BindableProperty.Create("Label", typeof(string),
+            typeof(DateCell));
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set
+            {
+                SetValue(LabelProperty, value);
+            }
+        }
+        public static readonly BindableProperty ValueProperty = BindableProperty.Create("Value", typeof(DateTime), typeof(DateCell));
+        public DateTime Value
+        {
+            get => (DateTime)GetValue(ValueProperty);
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        public DateCell()
+        {
+            InitializeComponent();
+        }
+
+        //protected override void OnPropertyChanged(string propertyName)
+        //{
+        //    if (propertyName == "Label")
+        //        label.Text = Label;
+        //    else if (propertyName == "Value")
+        //        datePicker.Date = Value;
+        //}
+
+
+        private static void HandleLabelValueChange(BindableObject bindable, object oldValue, object newValue)
+        {
+            var targetView = (DateCell)bindable;
+            if (targetView != null)
+                targetView.label.Text = (string) newValue;
+        }
+
+    }
+}
--- Playground/Playground/TableViewPage.xaml.cs
+++ Playground/Playground/TableViewPage.xaml.cs
@@ -9,12 +9,25 @@ using Xamarin.Forms.Xaml;
 
 namespace Playground
 {
+    public class DateRange
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string StartLabel { get; set; }
+    }
+
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class TableViewPage : ContentPage
 	{
 		public TableViewPage ()
 		{
 			InitializeComponent ();
-		}
+            BindingContext = new DateRange
+            {
+                Start = DateTime.Now,
+                End = DateTime.Now.AddDays(1),
+                StartLabel = "Start"
+            };
+        }
 	}
 }
